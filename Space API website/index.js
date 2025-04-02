76834e66055ffa5a10f0d8a1528dad369dbc3cf6
Ayşe <ayse.delen@yandex.com>
2025-04-02 14:58:32 +0300

    Get info from the NASA API and display as text

--- Space API website/index.js
+++ Space API website/index.js
@@ -3,11 +3,16 @@ import axios from "axios";
 
 const app =  express();
 const port = 3000;
+const API_KEY = "AsRPIBeeAng8K1210CFFMlGUGwRsT992cZjPumby";
+const URL = "https://api.nasa.gov/planetary/apod?api_key=";
 
 app.use(express.static("public"));
 
-app.get("/", (req,res)=>{
-    res.render("index.ejs");
+app.get("/", async (req,res)=>{
+    const source = await axios.get(URL + API_KEY);
+    const result = source.data;
+    let strResult = JSON.stringify(result)
+    res.render("index.ejs", {content: strResult});
 });
 
 app.listen(port, ()=>{
